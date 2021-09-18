e4b4e743a543d39ec3c9a8e5f9aeaa198ca42a6f
Cyebukayire <peacecyebukayire@gmail.com>
2021-09-18 20:33:26 +0200

    Add requirements and procfile

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+# import django_heroku
+# import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +27,7 @@ SECRET_KEY = 'h+o4%o5&_q8$n=!l$1n$z2_qot+9s^b2gpt8s8$k+bb2665ar%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
